87a7cb4ec4bd7a34fc19bbf58403ca646e9e7493
Lauri Kääriäinen <lauri.v.kaariainen@gmail.com>
2015-05-12 23:34:39 +0300

    now asks a question whether to create logfile if not found

--- ping-log.js
+++ ping-log.js
@@ -3,13 +3,63 @@ var path = require("path");
 var url = require("url"); 
 var fs = require("fs");
 var http = require("http");
+var readline = require('readline');
+
+
+
+
+//first run in folder?
+try{
+	//this should throw ENOENT if logfile doesn't exist
+	cleanUp();
+	//once in hour do cleanup in logfile 
+	setInterval(cleanUp,1000*60*60);
+
+	setInterval(logPing,1000*10);
+
+	http.createServer(ownHttpServer).listen(8893);
+	sys.puts("Server Running on 8893");	
+}
+catch(e){
+	//file did not exist
+	if(String(e).match("ENOENT")){
+		var rl = readline.createInterface({
+		  input: process.stdin,
+		  output: process.stdout
+		});
+
+		rl.question("Log file: 'ping-log.json' was not found in folder."+
+				" Press 'y' to create it and continue?\n", 
+			function(answer) {
+				
+				if(answer !== "y"){
+					process.exit(1);
+				}
+				
+				rl.close();
+				fs.writeFileSync("ping-log.json",'{"logs":[{"t":'+new Date().getTime()+',"ping":-1}');
+				
+				//once in hour do cleanup in logfile 
+				setInterval(cleanUp,1000*60*60);
+
+				setInterval(logPing,1000*10);
+			
+				http.createServer(ownHttpServer).listen(8893);
+				sys.puts("Server Running on 8893");	
+			}
+		);
+		
+
+	}
+}
+
 
 //oh wow this function looks horrible
 // <- me after a year of making this
 //this should be cleaning the logfile, because we won't need 
 //the frequency of 10s from old measurements, rather just one measurement per hour
 function cleanUp() {
-	var logJson = JSON.parse(fs.readFileSync("ping-log.txt",{encoding:"UTF-8"}) + "]}");
+	var logJson = JSON.parse(fs.readFileSync("ping-log.json",{encoding:"UTF-8"}) + "]}");
 	for(var i = 0;i < logJson.logs.length;i++){
 				if(logJson.logs[i].t < (new Date().getTime() - 1000*60*60)){
 					if(new Date(logJson.logs[i].t).getMinutes() === 0){
@@ -33,13 +83,12 @@ function cleanUp() {
 					}
 				}
 	}
-	fs.writeFileSync("ping-log.txt",JSON.stringify(logJson).substring(0,JSON.stringify(logJson).length-2));
+	fs.writeFileSync("ping-log.json",JSON.stringify(logJson).substring(0,JSON.stringify(logJson).length-2));
 }
-cleanUp();
 
 
-//once in hour do cleanup in logfile 
-setInterval(cleanUp,1000*60*60);
+
+
 
 //need to use 'spawn' instead of 'exec' to be able to use streams
 
@@ -50,7 +99,7 @@ function logPing(){
 	child.stdout.on('data', function(chunk) {
 		chunk = String(chunk);
 		if(chunk.match('1 received')) {
-			fs.appendFileSync("ping-log.txt",
+			fs.appendFileSync("ping-log.json",
 				",{\"t\":"+
 				(new Date()).getTime() +
 				",\"ping\":" + 
@@ -58,7 +107,7 @@ function logPing(){
 				"}");
 		}
 		else {
-			fs.appendFileSync("ping-log.txt", 
+			fs.appendFileSync("ping-log.json", 
 				",{\"t\":"+
 				(new Date()).getTime() + 
 				",\"ping\":" +
@@ -70,9 +119,11 @@ function logPing(){
 	});
 }
 
-setInterval(logPing,1000*10);
 
-http.createServer(function(request,response){
+
+
+
+function ownHttpServer(request,response){
 	
 	var my_path = url.parse(request.url).pathname;
 	//console.log("request.url: "+request.url);
@@ -83,13 +134,13 @@ http.createServer(function(request,response){
 		//sys.puts("GET!\nmypath:"+my_path);
 		if(my_path.match(/^\/ping\/all$/)){
 			response.writeHeader(200, {"Content-Type": "application/json"});
-			response.write(fs.readFileSync("ping-log.txt",{encoding:"UTF-8"}) + "]}");
+			response.write(fs.readFileSync("ping-log.json",{encoding:"UTF-8"}) + "]}");
 			response.end();
 			return;
 		}
 		else if(my_path.match(/^\/ping\/hour$/)){
 			
-			var logJson = JSON.parse((fs.readFileSync("ping-log.txt",{encoding:"UTF-8"}) + "]}"));
+			var logJson = JSON.parse((fs.readFileSync("ping-log.json",{encoding:"UTF-8"}) + "]}"));
 			//console.log("%i",logJson.logs);
 			for(var i = 0;i < logJson.logs.length;i=i){
 				if(logJson.logs[i].t < (new Date().getTime() - 1000*60*60))
@@ -104,7 +155,7 @@ http.createServer(function(request,response){
 			return;
 		}
 		else if(my_path.match(/^\/ping\/week$/)){
-			var logJson = JSON.parse((fs.readFileSync("ping-log.txt",{encoding:"UTF-8"}) + "]}"));
+			var logJson = JSON.parse((fs.readFileSync("ping-log.json",{encoding:"UTF-8"}) + "]}"));
 			for(var i = 0;i < logJson.logs.length;i = i=i){
 				if(logJson.logs[i].t < (new Date().getTime() - 1000*60*60*24*7))
 					logJson.logs.splice(i,1);
@@ -134,5 +185,4 @@ http.createServer(function(request,response){
 	}
 	
 
-}).listen(8893);
-sys.puts("Server Running on 8893");			
+}		
